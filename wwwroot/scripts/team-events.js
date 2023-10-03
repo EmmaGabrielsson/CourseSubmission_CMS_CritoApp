682f2cd2fcddd36a40c31afee16dafdba8164892
Emma Gabrielsson <111791152+dreamworks0601@users.noreply.github.com>
2023-10-03 09:30:14 +0200

    updated bootstrap links and fixed activee class on page

--- wwwroot/scripts/team-events.js
+++ wwwroot/scripts/team-events.js
@@ -1,13 +1,12 @@
 
- 
     document.addEventListener("DOMContentLoaded", function() {
         // Check if there is a 'page' query parameter
         var queryParams = new URLSearchParams(window.location.search);
-        var pageParam = queryParams.get("page");
+        var pageParam = queryParams.get("slide");
 
         if (pageParam) {
             // Scroll to the 'small-article-section' div when the page loads
-            var targetSection = document.getElementById("small-article-section");
+            var targetSection = document.getElementById("team-section");
 
             if (targetSection) {
                 var offsetTop = targetSection.getBoundingClientRect().top + window.scrollY;
