08a24789ca949f4ad7f493cb9521913793b61ae2
Emma Gabrielsson <111791152+dreamworks0601@users.noreply.github.com>
2023-10-06 19:25:02 +0200

    fixed validation script to handle validation on forms before submitting

---
+++ wwwroot/scripts/validation.js
@@ -0,0 +1,37 @@
+
+let validationElements = document.querySelectorAll('[data-val="true"]')
+
+for (let element of validationElements) {
+    element.addEventListener("keyup", function (e) {
+        switch (e.target.type) {
+            case 'text':
+                textValidator(e.target, 2)
+                break;
+            case 'textarea':
+                textValidator(e.target, 12)
+                break;
+            case 'email':
+                emailValidator(e.target)
+                break;
+        }
+    })
+}
+
+
+function textValidator(target, minLength) {
+    if (target.value.length < minLength) {
+
+        document.getElementById(`${target.id}`).previousElementSibling.innerHTML = "Too short, invalid input"
+    }
+    else
+        document.getElementById(`${target.id}`).previousElementSibling.innerHTML = ""
+}
+
+function emailValidator(target) {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (!emailRegex.test(target.value))
+        document.getElementById(`${target.id}`).previousElementSibling.innerHTML = "invalid email"
+    else
+        document.getElementById(`${target.id}`).previousElementSibling.innerHTML = ""
+}
