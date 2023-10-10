d5fcd8ba5ef77d0a18f057ffb233abc16c7d38bf
Emma Gabrielsson <111791152+dreamworks0601@users.noreply.github.com>
2023-10-10 10:51:58 +0200

    fixed more on validation before submit a form

--- wwwroot/scripts/validation.js
+++ wwwroot/scripts/validation.js
@@ -1,37 +1,69 @@
+document.addEventListener("DOMContentLoaded", function () {
 
-let validationElements = document.querySelectorAll('[data-val="true"]')
+    let subscribeForm, contactForm;
+    let emailValid = false;
+    let nameValid = false;
+    let messageValid = false;
 
-for (let element of validationElements) {
-    element.addEventListener("keyup", function (e) {
-        switch (e.target.type) {
-            case 'text':
-                textValidator(e.target, 2)
-                break;
-            case 'textarea':
-                textValidator(e.target, 12)
-                break;
-            case 'email':
-                emailValidator(e.target)
-                break;
-        }
-    })
-}
-
-function textValidator(target, minLength) {
-    if (target.value.length < minLength) {
-
-        document.getElementById(`${target.id}`).previousElementSibling.innerHTML = "Too short, invalid input"
+    let validationElements = document.querySelectorAll('[data-val="true"]');
+    for (let element of validationElements) {
+        element.addEventListener("keyup", function (e) {
+            switch (e.target.type) {
+                case 'text':
+                    nameValid = textValidator(e.target, 2); // Minimum 2 characters for the name
+                    break;
+                case 'textarea':
+                    messageValid = textValidator(e.target, 12); // Minimum 12 characters for the message
+                    break;
+                case 'email':
+                    emailValid = emailValidator(e.target);
+                    break;
+            }
+        });
     }
-    else
-        document.getElementById(`${target.id}`).previousElementSibling.innerHTML = ""
-}
 
-function emailValidator(target) {
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    contactForm = document.getElementById("contact-form");
+    subscribeForm = document.getElementById("subscribe-form");
 
-    if (!emailRegex.test(target.value))
-        document.getElementById(`${target.id}`).previousElementSibling.innerHTML = "invalid email"
-    else
-        document.getElementById(`${target.id}`).previousElementSibling.innerHTML = ""
-}
+    if (contactForm != null) {
+        contactForm.addEventListener("submit", function (event) {
+            event.preventDefault();
+            // If validation passes, submit the form
+            if (emailValid && nameValid && messageValid) {
+                contactForm.submit();
+            }
+        });
+    }
 
+    if (subscribeForm != null) {
+        subscribeForm.addEventListener("submit", function (event) {
+            event.preventDefault();
+            // If validation passes, submit the form
+            if (emailValid) {
+                subscribeForm.submit();
+            }
+        });
+    }
+
+    function emailValidator(emailInput) {
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+        if (!emailRegex.test(emailInput.value)) {
+            emailInput.previousElementSibling.innerHTML = "Invalid email";
+            return false;
+        } else {
+            emailInput.previousElementSibling.innerHTML = "";
+            return true;
+        }
+    }
+
+    function textValidator(target, minLength) {
+        if (target.value.length < minLength) {
+            document.getElementById(`${target.id}`).previousElementSibling.innerHTML = "Too short, invalid input";
+            return false;
+        } else {
+            document.getElementById(`${target.id}`).previousElementSibling.innerHTML = "";
+            return true;
+        }
+    }
+});
